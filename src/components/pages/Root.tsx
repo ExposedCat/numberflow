63093ae0a8102a19ce98b66b5a78f723daa4553d
Artem Prokop <artem13.prokop@gmail.com>
2025-09-14 23:58:28 +0200

    feat: flowchart engine

--- src/components/pages/Root.tsx
+++ src/components/pages/Root.tsx
@@ -1,13 +1,57 @@
-import type { FC } from "react";
-import { Button } from "@/components/ui/Button.tsx";
+import {
+	addEdge,
+	applyEdgeChanges,
+	applyNodeChanges,
+	type Connection,
+	type Edge,
+	type EdgeChange,
+	type Node,
+	type NodeChange,
+	ReactFlow,
+} from "@xyflow/react";
+import { type FC, useCallback, useState } from "react";
 import { Box } from "../ui/Box";
-import { StyledText } from "../ui/Typography";
+
+import "@xyflow/react/dist/style.css";
+import { Button } from "../ui/Button";
 
 export const Root: FC = () => {
+	const [nodes, setNodes] = useState<Node[]>([]);
+	const [edges, setEdges] = useState<Edge[]>([]);
+
+	const onNodesChange = useCallback(
+		(changes: NodeChange[]) =>
+			setNodes((prev) => applyNodeChanges(changes, prev)),
+		[],
+	);
+	const onEdgesChange = useCallback(
+		(changes: EdgeChange[]) =>
+			setEdges((prev) => applyEdgeChanges(changes, prev)),
+		[],
+	);
+	const onConnect = useCallback(
+		(connection: Connection) => setEdges((prev) => addEdge(connection, prev)),
+		[],
+	);
+
 	return (
-		<Box column center>
-			<StyledText variant="heading">React Essential</StyledText>
-			<Button label="Click me" />
+		<Box column center style={{ width: "100vw", height: "100vh" }}>
+			<Button
+				label="Add Node"
+				onClick={() =>
+					setNodes((prev) => [
+						...prev,
+						{
+							id: Date.now().toString(),
+							position: { x: 0, y: 200 },
+							data: { label: Date.now().toString() },
+						},
+					])
+				}
+			/>
+			<ReactFlow
+				{...{ nodes, edges, onNodesChange, onEdgesChange, onConnect }}
+			/>
 		</Box>
 	);
 };
